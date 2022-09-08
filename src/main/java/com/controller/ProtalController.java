ace5d19f5a0831dd0222c578d9d42b6d19484313
Layne <Layne@qq.com>
2022-09-08 21:32:04 +0800

    third master commit

--- src/main/java/com/controller/ProtalController.java
+++ src/main/java/com/controller/ProtalController.java
@@ -15,6 +15,7 @@ public static void main(String[] args) {
     System.out.println("hello hot-fix!");
     System.out.println("hello master test!");
     System.out.println("hello hot-fix  test!");
+    System.out.println("hello push-commit  test!");
 
 
 }
